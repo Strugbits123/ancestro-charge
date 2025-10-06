2ea98e208caf53a45e0b7ea9e90b9be2b92bc997
zainulabideen-strugbits <zainulabideen@strugbitsglobal.com>
2025-10-06 18:08:09 +0500

    Update CustomCheckbox.tsx
    fixed the tick icon

--- src/app/components/CustomCheckbox.tsx
+++ src/app/components/CustomCheckbox.tsx
@@ -1,88 +1,45 @@
-// components/CustomCheckbox.tsx
+
 import React from "react";
+import Image from "next/image";
 
 interface CustomCheckboxProps {
   checked: boolean;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   id?: string;
   label?: string;
+  tickIcon?: string;
 }
 
-const CustomCheckbox: React.FC<CustomCheckboxProps> = ({ checked, onChange, id, label }) => {
+const CustomCheckbox: React.FC<CustomCheckboxProps> = ({
+  checked,
+  onChange,
+  id,
+  label,
+  tickIcon = "/tick.svg",
+}) => {
   return (
-    <>
-      <style>{`
-        .custom-checkbox {
-          position: relative;
-        }
-        .custom-checkbox:checked::after {
-          content: '';
-          position: absolute;
-          width: 5px;
-          height: 10px;
-          border: solid #F8B03B;
-          border-width: 0 2px 2px 0;
-          transform: rotate(45deg);
-          top: 3px;
-          left: 6px;
-        }
-      `}</style>
-      <label className="inline-flex items-center gap-2 cursor-pointer">
+    <label className="inline-flex items-center gap-2 cursor-pointer select-none">
+      <div className="relative flex items-center justify-center w-[19px] h-[19px]">
         <input
           id={id}
           type="checkbox"
           checked={checked}
           onChange={onChange}
-          className="w-[19px] h-[19px] rounded-[5px] border border-white bg-[#FFFFFF33] appearance-none cursor-pointer custom-checkbox"
+          className="absolute w-full h-full rounded-[5px] border border-white bg-[#FFFFFF33] appearance-none cursor-pointer"
         />
-        {label && <span className="text-[14px]">{label}</span>}
-      </label>
-    </>
+        {checked && (
+          <Image
+            src={tickIcon}
+            alt="checked"
+            width={10}
+            height={10}
+            className="pointer-events-none"
+          />
+        )}
+      </div>
+      {label && <span className="text-[14px] text-white">{label}</span>}
+    </label>
   );
 };
 
 export default CustomCheckbox;
-// import React from "react";
-// import Image from "next/image";
-
-// interface CustomCheckboxProps {
-//   checked: boolean;
-//   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-//   id?: string;
-//   label?: string;
-//   tickIcon?: string; // optional path for custom tick icon
-// }
-
-// const CustomCheckbox: React.FC<CustomCheckboxProps> = ({
-//   checked,
-//   onChange,
-//   id,
-//   label,
-//   tickIcon = "/tick.svg", // default icon path
-// }) => {
-//   return (
-//     <label className="inline-flex items-center gap-2 cursor-pointer select-none">
-//       <div className="relative">
-//         <input
-//           id={id}
-//           type="checkbox"
-//           checked={checked}
-//           onChange={onChange}
-//           className="w-[19px] h-[19px] rounded-[5px] border border-white bg-[#FFFFFF33] appearance-none cursor-pointer"
-//         />
-//         {checked && (
-//           <Image
-//             src={tickIcon}
-//             alt="checked"
-//             width={12}
-//             height={12}
-//             className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pointer-events-none"
-//           />
-//         )}
-//       </div>
-//       {label && <span className="text-[14px] text-white">{label}</span>}
-//     </label>
-//   );
-// };
-
-// export default CustomCheckbox;
